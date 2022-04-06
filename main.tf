11f036c683fc8a4325dd77876bb9de233e2930cb
Matt Trachier <matttrach@gmail.com>
2022-04-06 17:11:35 -0500

    giving up

--- main.tf
+++ main.tf
@@ -1,23 +1,29 @@
 # NOTE: LIBVIRT_DEFAULT_URI needs to be set, probably to qemu:///session (validate with 'virsh uri' command)
+provider "libvirt" {
+  uri = "qemu+unix:///session?socket=/opt/homebrew/var/run/libvirt/libvirt-sock"
+}
 
 resource "libvirt_domain" "default" {
   name = "U20ARM"
   description = "Ubuntu 20.04 LTS ARM version"
   running = "true"
-  cpu = "host-passthrough"
   vcpu = "2"
-  memory = "2G"
+  memory = "2048"
   arch = "aarch64"
   machine = "virt,highmem=off"
   cloudinit = ""
-  boot_device = ""
+  boot_device {
+    dev = ["hd"]
+  }
+  cpu {
+    mode = "host-passthrough"
+  }
   disk {
       volume_id = ""
   }
   network_interface {
       network_id = ""
   }
-
   # IMPORTANT: this is a known bug on cloud images
   # https://bugs.launchpad.net/cloud-images/+bug/1573095
   console {
@@ -25,13 +31,11 @@ resource "libvirt_domain" "default" {
     target_port = "0"
     target_type = "serial"
   }
-
   console {
     type        = "pty"
     target_type = "virtio"
     target_port = "1"
   }
-
   graphics {
     type        = "spice"
     listen_type = "address"
